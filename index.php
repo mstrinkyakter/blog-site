bb5eecf18b9d24b7c44797dd02299e5a65218ec2
Mst Rinky Akter <mstrinkya@gmail.com>
2023-10-15 13:42:35 +0600

    featured and aside section

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,43 +8,141 @@
     <!-- boostrap link  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-<ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Web Design</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Graphic Design</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Networking</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Language</a>
-        </li>
-</ul>
-        <a href="">login</a>
-    </div>
-  </div>
-</nav> 
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-center">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Web Design</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Graphic Design</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Networking</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Language</a>
+                        </li>
+                    </ul>
+                    <a href="">login</a>
+                </div>
+            </div>
+        </nav>
     </header>
     <main>
+        <section class="py-5">
+            <div class="container">
+                <div class="row g-5">
+                    <article class="col-12 col-lg-8">
+                        <h1 class="text-center">Featured Posts</h1>
+                        <div class="row gy-4">
+                            <div class="col-12">
+                                <div class="card">
+                                    <img src="https://miro.medium.com/v2/resize:fit:915/0*4DpFVUP_VfkhzSIL" alt=""
+                                        height="300px" width="100%px">
+                                    <div class="card-body">
+                                        <h3>Lorem ipsum dolor sit.</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, a?</p>
+                                        <a href="">Read More...</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="card">
+                                    <img src="https://miro.medium.com/v2/resize:fit:915/0*4DpFVUP_VfkhzSIL" alt=""
+                                        height="300px" width="100%px">
+                                    <div class="card-body">
+                                        <h3>Lorem ipsum dolor sit.</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, a?</p>
+                                        <a href="">Read More...</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="card">
+                                    <img src="https://miro.medium.com/v2/resize:fit:915/0*4DpFVUP_VfkhzSIL" alt=""
+                                        height="300px" width="100%px">
+                                    <div class="card-body">
+                                        <h3>Lorem ipsum dolor sit.</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, a?</p>
+                                        <a href="">Read More...</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                    <aside class="col-12 col-lg-4">
+                        <div class="row g-4">
+                            <div class="col-12 ">
+                              <h3 class="mt-4">Featured Categories</h3>
+                               
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">An item</li>
+                                        <li class="list-group-item">A second item</li>
+                                        <li class="list-group-item">A third item</li>
+                                        <li class="list-group-item">A fourth item</li>
+                                        <li class="list-group-item">And a fifth one</li>
+                                    </ul>
+                                
+                            </div>
+                            <div class="col-12 ">
+                              <h3 class="mt-4">Featured Categories</h3>
+                               
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">An item</li>
+                                        <li class="list-group-item">A second item</li>
+                                        <li class="list-group-item">A third item</li>
+                                        <li class="list-group-item">A fourth item</li>
+                                        <li class="list-group-item">And a fifth one</li>
+                                    </ul>
+                                
+                            </div>
+                        </div>
 
+                    </aside>
+                </div>
+                <br>
+                <!-- pagination  -->
+                <nav aria-label="Page  navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <section>
+
+        </section>
     </main>
-    <footer>
-
+    <footer class="bg-dark text-center py-5 text-light">
+        <p>&copy; 2023 Elearning | All Rights Reserved</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
